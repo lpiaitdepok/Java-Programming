fc230650b0b68b8cb91e5d5db53b1e514e010828
lpiaitdepok <lpiaitdepok@gmail.com>
2018-09-29 10:13:18 +0700

    Create GetCharacter.java

---
+++ latihan/GetCharacter.java
@@ -0,0 +1,10 @@
+class GetCharacter
+{
+  public static void main(String args[]) throws Exception
+  {
+	char c;
+	System.out.println("Enter a character:");
+	c = (char)System.in.read();
+	System.out.println("You entered "+c);
+  }
+}
