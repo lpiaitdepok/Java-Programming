488200b4e5155e93f3fceb3a10129f2d696f82f5
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-11 10:26:08 +0700

    Update SumArray.java

--- latihan/snippet/SumArray.java
+++ latihan/snippet/SumArray.java
@@ -5,3 +5,11 @@ for (int i=0; i < a.length; i==) // note the use of the length variable
 total += a[i];
 return total;
 }
+
+
+/* implementation */
+int[] intArray = {1, 2, 3};
+/* or */
+int[] intArray = new int[] {1, 2, 3};
+
+System.out.println(SumArray(intArray));
