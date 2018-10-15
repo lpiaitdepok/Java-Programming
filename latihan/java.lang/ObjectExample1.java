eb4018e7cfba0c07ea6e7d845b82ee6dc1585959
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-15 09:49:32 +0700

    Create ObjectExample1.java

---
+++ latihan/java.lang/ObjectExample1.java
@@ -0,0 +1,16 @@
+public class ObjectExample1
+{
+    public static void main(String args[])
+    {
+        //Object CLASS DECLARATION
+        Object obj;
+ 
+        //CREATE INSTANCE FOR Object CLASS
+        obj = new Object();
+ 
+        //Object CLASS OUTPUT
+        //NOTHING TO WORRRY THERE IS NO VALUE
+        //IT PRINTS SOME CODE
+        System.out.println("Object value : " + obj);
+    }
+}
