3828eb93131e39ae6971c7816ce740060ce561f3
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-15 09:04:47 +0700

    Create StrictMathExample2.java

---
+++ latihan/java.lang/StrictMathExample2.java
@@ -0,0 +1,39 @@
+// http://www.hudatutorials.com
+public class StrictMathExample2
+{
+    public static void main(String args[])
+    {
+        //RETURNS ARC COSINE
+        System.out.println("acos : " + StrictMath.acos(0.4));
+ 
+        //RETURNS ARC SINE
+        System.out.println("asin : " + StrictMath.asin(0.4));
+ 
+        //RETURNS ARC TANGENT VALUE
+        System.out.println("atan : " + StrictMath.atan(45));
+ 
+        //RETURNS COSINE OF AN ANGLE
+        System.out.println("cos : " + StrictMath.cos(45));
+ 
+        //RETURNS HYPERBOLIC COSINE
+        System.out.println("cosh : " + StrictMath.cosh(45));
+ 
+        //RETURNS SINE OF AN ANGLE
+        System.out.println("sin : " + StrictMath.sin(45));
+ 
+        //RETURNS HYPERBOLIC SINE
+        System.out.println("sinh : " + StrictMath.sinh(45));
+ 
+        //RETURNS TANGENT OF AN ANGLE
+        System.out.println("tan : " + StrictMath.tan(45));
+ 
+        //RETURNS HYPERBOLIC TANGENT
+        System.out.println("tanh : " + StrictMath.tanh(45));
+ 
+        //RETURNS DEGREES
+        System.out.println("toDegrees : " + StrictMath.toDegrees(45));
+ 
+        //RETURNS RADIANS
+        System.out.println("toRadians : " + StrictMath.toRadians(45));
+    }
+}
