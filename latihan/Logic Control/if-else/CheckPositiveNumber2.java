c7d72b613a2df340dfc3b19dd7fc13a8986a7457
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-15 12:04:34 +0700

    Create CheckPositiveNumber2.java

---
+++ latihan/Logic Control/if-else/CheckPositiveNumber2.java
@@ -0,0 +1,15 @@
+// www.programiz.com
+class CheckPositiveNumber2 {
+   public static void main(String[] args) {    	
+      int number = 10;
+	 
+      if (number > 0) {
+         System.out.println("Number is positive.");
+      }
+      else {
+         System.out.println("Number is not positive.");
+      }
+ 
+      System.out.println("This statement is always executed.");
+   }
+}
