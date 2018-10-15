417edae55d8fcea59ec8d5d0e188d823b1821af0
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-15 15:19:31 +0700

    Create StringSwitchDemo.java

---
+++ latihan/Logic Control/switch-case/StringSwitchDemo.java
@@ -0,0 +1,38 @@
+public class StringSwitchDemo {
+    public static void main(){
+
+        String month = "August";
+        int monthNumber = 0;
+
+        switch (month.toLowerCase()) {
+            case "january":    monthNumber =  1;
+				break;
+            case "february":   monthNumber =  2;
+				break;
+            case "march":      monthNumber =  3;
+				break;
+            case "april":      monthNumber =  4;
+				break;
+            case "may":        monthNumber =  5;
+				break;
+            case "june":       monthNumber =  6;
+				break;
+            case "july":       monthNumber =  7;
+				break;
+            case "august":     monthNumber =  8;
+				break;
+            case "september":  monthNumber =  9;
+				break;
+            case "october":    monthNumber = 10;
+				break;
+            case "november":   monthNumber = 11;
+				break;
+            case "december":   monthNumber = 12;
+				break;
+            default:           monthNumber =  0;
+				break;
+        }
+
+        System.out.println(monthNumber);
+    }
+}
