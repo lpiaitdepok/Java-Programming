3d62bdd0ef19685379f405781ea23b63adaa732e
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-15 08:32:00 +0700

    Create MathExample2.java

---
+++ latihan/java.lang/MathExample2.java
@@ -0,0 +1,39 @@
+/http://www.hudatutorials.com/
+public class MathExample2
+{
+    public static void main(String args[])
+    {
+        //RETURNS ARC COSINE
+        System.out.println("acos : " + Math.acos(0.4));
+ 
+        //RETURNS ARC SINE
+        System.out.println("asin : " + Math.asin(0.4));
+ 
+        //RETURNS ARC TANGENT VALUE
+        System.out.println("atan : " + Math.atan(45));
+ 
+        //RETURNS COSINE OF AN ANGLE
+        System.out.println("cos : " + Math.cos(45));
+ 
+        //RETURNS HYPERBOLIC COSINE
+        System.out.println("cosh : " + Math.cosh(45));
+ 
+        //RETURNS SINE OF AN ANGLE
+        System.out.println("sin : " + Math.sin(45));
+ 
+        //RETURNS HYPERBOLIC SINE
+        System.out.println("sinh : " + Math.sinh(45));
+ 
+        //RETURNS TANGENT OF AN ANGLE
+        System.out.println("tan : " + Math.tan(45));
+ 
+        //RETURNS HYPERBOLIC TANGENT
+        System.out.println("tanh : " + Math.tanh(45));
+ 
+        //RETURNS DEGREES
+        System.out.println("toDegrees : " + Math.toDegrees(45));
+ 
+        //RETURNS RADIANS
+        System.out.println("toRadians : " + Math.toRadians(45));
+    }
+}
