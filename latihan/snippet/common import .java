408a200960243c885056f5679032b780948af61c
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-16 12:26:15 +0700

    Create common import .java

---
+++ latihan/snippet/common import .java
@@ -0,0 +1,7 @@
+import java.awt.*;	Common GUI elements.
+import java.awt.event.*;	The most common GUI event listeners.
+import javax.swing.*;	More common GUI elements. Note "javax".
+import java.util.*;	Data structures (Collections), time, Scanner, etc classes.
+import java.io.*;	Input-output classes.
+import java.text.*;	Some formatting classes.
+import java.util.regex.*;	Regular expression classes.
