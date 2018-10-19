baafa2dc29e9897a1e33773c56b4c0324a996986
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-19 19:13:54 +0700

    Create SimpleIf.java

---
+++ latihan/Logic Control/if-else/SimpleIf.java
@@ -0,0 +1,19 @@
+public class SimpleIf {
+
+  public static void main (String[] args) {
+    int a = 10;
+    int b = 20;
+
+    if(a>b) {
+      System.out.println("a is greater than b");
+    }
+	
+    if(a<b) {
+      System.out.println("a is less than b");
+    }
+	
+    if(a==b) {
+      System.out.println("a is equal to b");
+    }
+  }
+}
