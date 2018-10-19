a6e443bfe376af8e3a14c80c34ce597bfbd979ab
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-19 18:49:13 +0700

    Create ArithmeticDemo2.java

---
+++ latihan/java.lang/ArithmeticDemo2.java
@@ -0,0 +1,23 @@
+class ArithmeticDemo2 {
+
+     public static void main (String[] args){
+          int a = 9;
+          int b = 13;
+		  
+          int result = a + b; // result is now 22
+          System.out.println(result);
+
+          result = a - b; // result is now -4
+          System.out.println(result);
+
+          result = a * b; // result is now 117
+          System.out.println(result);
+
+          result = a / b; // result is now 0
+          System.out.println(result);
+
+          result = a % b; // result is now 9
+          System.out.println(result);
+
+     }
+}
