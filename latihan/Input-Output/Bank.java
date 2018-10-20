3615ba7ddc35080819d21b7b6102cf316c3f24e6
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-20 13:50:28 +0700

    Create Bank.java

---
+++ latihan/Input-Output/Bank.java
@@ -0,0 +1,60 @@
+/** @author DoberFrann */
+
+import java.util.*;
+
+public class Bank {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the Bank");
+    
+    int selection = 0;
+    
+    do {
+      System.out.println("[1] DISPLAY BALANCE");
+      System.out.println("[2] DEPOSIT MONEY");
+      System.out.println("[3] WITHDRAW MONEY");
+      System.out.println("[4] QUIT");
+      
+      System.out.println("Inser selection: ");
+      selection = ReadConsole.nextInt();
+      
+      switch (selection) {
+        case 1:
+          BankAccount.display();
+          break;
+        case 2:
+          BankAccount.deposit();
+          break;
+        case 3:
+          BankAccount.withdraw();
+          break;
+        case 4:
+          System.out.println("-------------------");
+          break;
+        default:
+          System.out.println("The selection is invalid");
+      }
+    } while (selection != 4);
+  }
+}
+
+public class ReadConsole {
+  public static int getInteger() {
+    try {
+      byte[] buffer = new byte[512];
+      System.in.read(buffer);
+      String s = new String(buffer);
+      s = s.trim();
+      
+      // We return an integer value
+      return Integer.parseInt(s);
+    } catch (Exception e) {
+      System.out.println("ERROR");
+    }
+    return 0;
+  }
+  public static int nextInt() {
+    Scanner keyboard = new Scanner(System.in);
+    int i = keyboard.nextInt();
+    return i;
+  }
+}
