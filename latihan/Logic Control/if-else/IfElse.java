5f1267a702fd7605b9a141c75f9d82a5aba18989
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-20 11:27:28 +0700

    Create IfElse.java

---
+++ latihan/Logic Control/if-else/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+    	int a = 23, b = 47, c;
+
+    	if (a > b) {
+    		System.out.println("a is greather than b.");
+    	} else {
+    	  System.out.println("a is less than or equal to b");
+      }
+    }
+}
