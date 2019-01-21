b2e23116c8f808ab17be9bdc5bdec7c182b324ba
lpiaitdepok <lpiaitdepok@gmail.com>
2019-01-21 20:18:16 +0700

    Create ArithmeticOperation1.java

---
+++ latihan/Input-Output/ArithmeticOperation1.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArithmeticOperation1 {
+public static void main (String[] args) throws IOException {
+InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+
+int a = Integer.parseInt(br.readLine());
+int b = Integer.parseInt(br.readLine());
+
+}
+}
