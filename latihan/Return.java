a95e55fa08a957cb64d28d10971a962410bc3640
lpiaitdepok <lpiaitdepok@gmail.com>
2019-01-23 17:48:02 +0700

    Update Return.java

--- latihan/Return.java
+++ latihan/Return.java
@@ -1,7 +1,8 @@
 public class Return {
 public static void main (String[] args) { 
-static int add(int n1, int n2) {
- 
+System.out.println(add(1,2));
 }
+ static int add(int n1, int n2) {
+ return n1+n2;
 }
 }
