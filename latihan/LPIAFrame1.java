24ad0332ccd30c0e350120390a0d97bf607f1cf6
lpiaitdepok <lpiaitdepok@gmail.com>
2019-01-29 15:27:18 +0700

    Update LPIAFrame1.java

--- latihan/LPIAFrame1.java
+++ latihan/LPIAFrame1.java
@@ -2,11 +2,13 @@ import javax.swing.JFrame;
 public class LPIAFrame1 extends JFrame {
   public static void main(String[] args) {
     //Create and set up the window.
-    JFrame frame = new JFrame("LPIA Frame");
+    LPIAFrame1 frame = new LPIAFrame1();
+    frame.setTitle("LPIA Frame");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     //Display the window.
-    frame.setSize(199,92);
+    frame.setSize(920,199);
+    // frame.pack();
     frame.setVisible(true);
   }    
 }
