2d23dee48f2690b43eb882b20f8e1a327c4dd0c2
lpiaitdepok <lpiaitdepok@gmail.com>
2019-01-29 15:22:04 +0700

    Create LPIAFrame.java

---
+++ latihan/LPIAFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class LPIAFrame extends JFrame {
+  public static void main(String[] args) {
+    //Create and set up the window.
+    JFrame frame = new JFrame("LPIA Frame");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    //Display the window.
+    frame.setSize(199,92);
+    frame.setVisible(true);
+  }    
+}
