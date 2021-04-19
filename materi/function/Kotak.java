be495932f03774543d139617c1d52185f3795d6a
lpiaitdepok <lpiaitdepok@gmail.com>
2021-04-19 11:37:19 +0700

    Create Kotak.java

---
+++ materi/function/Kotak.java
@@ -0,0 +1,9 @@
+class Kotak{
+	double panjang;
+	double lebar;
+	double tinggi;
+	 // Mendefinisikan method void (tidakmengembalikan nilai)
+	void cetakVolume(){
+		System.out.println("Volume kotak = "+ (panjang * lebar * tinggi));
+	}
+}
